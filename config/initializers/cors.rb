615f428e14bca05190020461d7f1bd3bca4a4d9f
Michael Kim <m.kim4247@gmail.com>
2019-08-02 14:38:27 -0400

    adding landing nav

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://on-it-app.herokuapp.com'
+    origins 'https://on-it-app.herokuapp.com', 'http://localhost:3000'
 
     resource '*',
       headers: :any,
